a83622737f5078df34de792fc6317e7eebceaac2
bertojuni <49329872+bertojuni@users.noreply.github.com>
2025-05-10 20:30:39 +0700

    move name app

--- resources/views/vendor/verify-new-email/verifyNewEmail.blade.php
+++ resources/views/vendor/verify-new-email/verifyNewEmail.blade.php
@@ -1,16 +1,16 @@
 @component('mail::message')
 # Verify New Email Address
 
-Terima kasih telah mendaftar di Kraflo.
+Terima kasih telah mendaftar di Rencanakan.
 Untuk menyelesaikan proses pendaftaran dan mulai menggunakan layanan kami, silakan verifikasi email Anda dengan mengklik tombol di bawah ini:
 
 @component('mail::button', ['url' => $url])
 Verify New Email Address
 @endcomponent
 
-Jika Anda tidak memperbarui alamat email di Kraflo, abaikan email ini.
+Jika Anda tidak memperbarui alamat email di Rencanakan, abaikan email ini.
 
 Terima kasih,<br>
 <!-- {{ config('app.name') }} -->
-Tim Kraflo
+Tim Rencanakan
 @endcomponent
